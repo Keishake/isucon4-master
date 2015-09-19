9a14cce29fbb1fd4fef11c14a0df96200439e02b
Keishake <keishake0925@gmail.com>
2015-09-19 13:31:20 +0900

    kill old process when unicorn restart

--- qualifier/webapp/ruby/unicorn_config.rb
+++ qualifier/webapp/ruby/unicorn_config.rb
@@ -1,2 +1,17 @@
 worker_processes 10
 preload_app true
+
+before_fork do |server, worker|
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+
+  sleep 1
+end
+
